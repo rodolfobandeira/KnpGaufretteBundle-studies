be6969fc1c7b69342767b00c41981e1cb2e87db6
rodolfobandeira <contato@rodolfobandeira.com>
2015-11-14 18:10:26 -0500

    Uploading and reading files from Amazon S3

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ KnpGaufretteBundle-studies
 ===
 
 ####Goal:
-1. Learn how to configure KnpGaufretteBundle and Symfony2 PHP Framework;
-2. Create Upload form and save images on Amazon S3;
-2. Run the same application on 2 web-nodes behind a loadbalancer HAProxy;
-3. Deploy the same application in both webservers;
-4. Disable web-node1 and check the availability of the system. Must be always online;
-5. Set triggers on Amazon CloudWatch to scale horizontally (Increase and Decrease number of webnodes automatically);
+- [x] Learn how to configure KnpGaufretteBundle and Symfony2 PHP Framework;
+- [x] Create Upload form and save images on Amazon S3;
+- [ ] Run the same application on 2 web-nodes behind a loadbalancer HAProxy;
+- [ ] Deploy the same application in both webservers;
+- [ ] Disable web-node1 and check the availability of the system. Must be always online;
+- [ ] Set triggers on Amazon CloudWatch to scale horizontally (Increase and Decrease number of webnodes automatically);
 
 A Symfony project created on November 13, 2015, 10:17 pm.
 
@@ -15,11 +15,25 @@ A Symfony project created on November 13, 2015, 10:17 pm.
 - `git clone https://github.com/rodolfobandeira/KnpGaufretteBundle-studies.git`
 - `cd KnpGaufretteBundle-studies`
 - `composer install --prefer-dist`
+- `mv app/config/amazon_s3.yml.dist app/config/amazon_s3.yml`
+- `vim app/config/amazon_s3.yml` (Edit your Amazon configuration)
 - `php app/console server:run`
 - `http://127.0.0.1:8000`
 
 ---
 
-Contact: 
+####Reference:
 
-[https://twitter.com/rodolfobandeira](https://twitter.com/rodolfobandeira)
+- [https://github.com/Elorfin/AS3AccessorSandbox](https://github.com/Elorfin/AS3AccessorSandbox) Got nice ideas like 
+adding all the config in just one file called: `app/config/amazon_s3.yml` :thumbsup:
+- [https://github.com/KnpLabs/KnpGaufretteBundle](https://github.com/KnpLabs/KnpGaufretteBundle) Official Bundle 
+repository. The full documentation is on their [README](https://github.com/KnpLabs/KnpGaufretteBundle/blob/master/README.markdown)
+- [https://github.com/KnpLabs/Gaufrette/blob/master/src/Gaufrette/Adapter/AmazonS3.php](https://github.com/KnpLabs/Gaufrette/blob/master/src/Gaufrette/Adapter/AmazonS3.php)
+To understand better how the AmazonS3 Adapter works.
+
+
+--- 
+
+####Contact: 
+
+[https://twitter.com/rodolfobandeira](https://twitter.com/rodolfobandeira) - Got a problem? Some idea? Drop me a message!
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AmazonS3;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,63 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $onlyFiles = [];
+        $amazonS3 = $this->get('knp_gaufrette.filesystem_map')->get('amazon');
+
+        // Create Upload form
+        $form = $this->createFormBuilder()
+            ->add('upload_file', 'file')
+            ->add('save', 'submit')
+            ->getForm();
+        if ($this->getRequest()->isMethod('POST')) {
+            // Process form if needed
+            $form->handleRequest($this->getRequest());
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $uploadedFile = $data['upload_file'];
+                $filename = $uploadedFile->getClientOriginalName();
+                try {
+                    $adapter = $amazonS3->getAdapter();
+                    $adapter->setAcl(AmazonS3::ACL_PRIVATE);
+                    $adapter->setMetadata($filename, array('contentType' => $uploadedFile->getClientMimeType()));
+                    $adapter->write($filename, file_get_contents($uploadedFile->getPathname()));
+                } catch(\Exception $e) {
+                    die($e->getMessage());
+                }
+            }
+        }
+        
+        
+        $files = $amazonS3->keys();
+
+        foreach($files as $file) {
+            if (preg_match('/\./s', $file)) {
+                $onlyFiles[] = $file;
+            }
+        }
+
+        $urlPrefix = sprintf("%s/%s/",
+            $this->container->getParameter('amazon_s3_url'),
+            $this->container->getParameter('amazon_s3_bucket')
+        );
+
+        return $this->render('AppBundle:default:index.html.twig', [
+            'files' => $onlyFiles,
+            'urlPrefix' => $urlPrefix,
+            'upload_form' => $form->createView()
+        ]);
     }
+
+
+    /**
+     * @Route("/upload", name="upload")
+     */
+    public function uploadAction(Request $request)
+    {
+        $amazonS3 = $this->get('knp_gaufrette.filesystem_map')->get('amazon');
+
+        die('<p>end_upload</p>');
+    }
+
+
 }
